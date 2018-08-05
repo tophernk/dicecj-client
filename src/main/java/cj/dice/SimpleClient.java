a6124766ef7f1c3699efa1eed1a0fdfb78b71c3e
tophernk <topher.nk@googlemail.com>
2018-08-05 15:22:38 +0200

    simple http request

--- src/main/java/cj/dice/SimpleClient.java
+++ src/main/java/cj/dice/SimpleClient.java
@@ -1,13 +1,20 @@
 package cj.dice;
 
-/**
- * Hello world!
- *
- */
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+
 public class SimpleClient
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Content content = null;
+        try {
+            content = Request.Get("http://localhost:8080").execute().returnContent();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println( content.asString() );
     }
 }
