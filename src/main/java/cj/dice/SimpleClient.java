baba936a41c0528d6dfd8788f39ad37a0106c014
tophernk <topher.nk@googlemail.com>
2018-08-05 12:44:17 +0200

    dicecj client

---
+++ src/main/java/cj/dice/SimpleClient.java
@@ -0,0 +1,13 @@
+package cj.dice;
+
+/**
+ * Hello world!
+ *
+ */
+public class SimpleClient
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cj/dice/SimpleClientTest.java
@@ -0,0 +1,20 @@
+package cj.dice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SimpleClientTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
